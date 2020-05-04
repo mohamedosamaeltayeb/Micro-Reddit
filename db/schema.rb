c1514699b1b6897c9ef416eb4d8c2e8e171dab01
Mohamedosamaeltayeb <mohamedosamaeltayeb@gmail.com>
2020-05-05 00:00:21 +0200

    Third commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_02_043145) do
+ActiveRecord::Schema.define(version: 2020_05_04_215434) do
 
   create_table "comments", force: :cascade do |t|
-    t.string "author_name"
     t.text "body"
     t.integer "user_id"
     t.integer "post_id"
